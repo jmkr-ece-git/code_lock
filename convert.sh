5c0c7af8fe9be78a66601529ffb85f1f7f17747f
Jesper Kristensen <jmkr@ece.au.dk>
2026-03-22 23:19:17 +0100

    modified:   convert.sh new file:   vhdl_ls.toml

--- convert.sh
+++ convert.sh
@@ -5,23 +5,26 @@ set -e
 cd $(dirname "$0")
 
 # NEORV32 home folder
-NEORV32_HOME=${NEORV32_RTL:-../neorv32}
+#SOURCE_HOME=${NEORV32_RTL:-../neorv32}
 # Additional sources (i.e. the top unit)
-SRC_FOLDER=${SRC_FOLDER:-.}
+#SRC_FOLDER=${SRC_FOLDER:-.}
 # Name of the top unit
-TOP=neorv32_verilog_wrapper
+#TOP=neorv32_verilog_wrapper
 
 mkdir -p build
-
+mkdir -p work
 # show NEORV32 version
-echo "NEORV32 Version:"
-grep -rni $NEORV32_HOME/rtl/core/neorv32_package.vhd -e 'hw_version_c'
-echo ""
-sleep 2
+#echo "NEORV32 Version:"
+#grep -rni $NEORV32_HOME/rtl/core/neorv32_package.vhd -e 'hw_version_c'
+#echo ""
+#sleep 2
 
 # Import and analyze sources
-ghdl -i --std=08 --work=neorv32 --workdir=build -Pbuild $NEORV32_HOME/rtl/core/*.vhd $SRC_FOLDER/$TOP.vhd
-ghdl -m --std=08 --work=neorv32 --workdir=build $TOP
+*ghdl -i --std=08 --work= --workdir=build -Pbuild $NEORV32_HOME/rtl/core/*.vhd $SRC_FOLDER/$TOP.vhd
+ghdl -i --std=08 --work=work --workdir=build -Pbuild ./*.vhd
+
+#ghdl -m --std=08 --work=neorv32 --workdir=build $TOP
+ghdl -m --std=08 --work=work --workdir=build $TOP
 # Synthesize Verilog
 ghdl synth --std=08 --work=neorv32 --workdir=build -Pbuild --out=verilog $TOP > $SRC_FOLDER/$TOP.v
 
